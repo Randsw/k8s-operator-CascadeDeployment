2a165f77a9120f7e4ad088c68cfc18fad8b2d9f7
Randsw <randsw@mail.ru>
2023-09-13 16:53:57 +0300

    fix: Use new field in manager Options

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	cascadev1alpha1 "github.com/randsw/cascadeAuto-operator/api/v1alpha1"
 	"github.com/randsw/cascadeAuto-operator/controllers"
@@ -68,9 +69,10 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: server.Options{
+			BindAddress: metricsAddr,
+		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "c82c3f0c.cascade.net",
