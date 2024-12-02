3ea5bd4483a6c7bab91e006c8b31dc1548f74c34
Randsw <randsw@mail.ru>
2024-12-02 18:33:32 +0300

    feat: Add new CR - JobRun

--- Makefile
+++ Makefile
@@ -49,7 +49,7 @@ endif
 # Image URL to use all building/pushing image targets
 IMG ?= controller:latest
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
-ENVTEST_K8S_VERSION = 1.30.0
+ENVTEST_K8S_VERSION = 1.31.0
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
@@ -188,7 +188,7 @@ ENVTEST ?= $(LOCALBIN)/setup-envtest
 
 ## Tool Versions
 KUSTOMIZE_VERSION ?= v5.4.1
-CONTROLLER_TOOLS_VERSION ?= v0.16.1
+CONTROLLER_TOOLS_VERSION ?= v0.16.5
 
 KUSTOMIZE_INSTALL_SCRIPT ?= "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"
 .PHONY: kustomize
---
+++ api/v1alpha1/jobrun_types.go
@@ -0,0 +1,45 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type CascadeRunSpec struct {
+
+	Object string `json:"object"`
+	Source string `json:"source"`
+	PackageID string `json:"packageid"`
+	ScenarioName string `json:"scenarioname"`
+	Modules []string `json:"modules"`
+}
+
+type CascadeRunStatus struct {
+	Result []string `json:"result"`
+	Info string `json:"info"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Job Result",type="string",JSONPath=".status.result",description="Jobs result"
+// +kubebuilder:printcolumn:name="Info",type="string",JSONPath=".status.info",description="Information"
+// CascadeAutoOperator is the Schema for the cascadeautooperators API
+type CascadeRun struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CascadeRunSpec   `json:"spec,omitempty"`
+	Status CascadeRunStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CascadeAutoOperatorList contains a list of CascadeAutoOperator
+type CascadeRunList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CascadeRun `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CascadeRun{}, &CascadeRunList{})
+}
